0c891ec05dcf2aab81dba57fcd649c6f4b79f260
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-06 15:55:43 +0700

    upload: index.mjs

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import cjsModule from "./index.js";
+
+export const brat = cjsModule.brat;
+export const bratSticker = cjsModule.bratSticker;
+export const bratVideo = cjsModule.bratVideo;
+export const bratVideoSticker = cjsModule.bratVideoSticker;
+export const themes = cjsModule.themes;
+export const fonts = cjsModule.fonts;
+export const emojis = cjsModule.emojis;
+
+export default cjsModule;
